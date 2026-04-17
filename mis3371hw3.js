6cdd63f0b0d161562aac400ac7860636ac0250b8
Faduran38 <faduran@cougarnet.uh.edu>
2026-04-17 12:01:22 -0500

    Update mis3371hw3.js

--- mis3371hw3.js
+++ mis3371hw3.js
@@ -15,6 +15,13 @@ function myFunction() { //Validates the ssn input, if it doesn't fit requirement
     else {
       x.type = "password";
     }
+    function myFunction() {
+      var x = document.getElementById("reenter");
+      if(x.type === "password") {
+        x.typw = "text";}
+      else {
+        x.type = "password";
+      }
     function myFunction() { //Used to mask the ssn 
       var x = document.getElementById("ssn");
       if(x.type === "password") 
